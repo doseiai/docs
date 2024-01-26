e91e4f3f88f1a9a64f8343d447ed0ff95a96d324
Alw3ys <alw3ys@gmail.com>
2024-01-26 16:43:03 +0100

    chore: star button dark theme

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -15,8 +15,8 @@ export default function Banner() {
       open ? "sm:flex": "hidden"
     )}>
       <div className="pointer-events-none fixed inset-x-0 bottom-0 sm:flex sm:justify-center sm:px-6 sm:pb-5 lg:px-8">
-        <div className="pointer-events-auto flex items-center justify-between gap-x-6 bg-zinc-900/80 px-6 py-2.5 sm:rounded-xl sm:py-3 sm:pl-4 sm:pr-3.5">
-          <p className="text-sm leading-6 text-white">
+        <div className="pointer-events-auto flex items-center justify-between gap-x-6 bg-zinc-900/80  dark:bg-white/80 px-6 py-2.5 sm:rounded-xl sm:py-3 sm:pl-4 sm:pr-3.5">
+          <p className="text-sm leading-6 text-white dark:text-black">
             Show your support by giving us a star!
           </p>
           <div className="flex items-center justify-center -mt-1 -mb-2.5">
@@ -30,7 +30,7 @@ export default function Banner() {
           </div>
           <button type="button" className="-m-1.5 flex-none p-1.5" onClick={() => setOpen(!open)}>
             <span className="sr-only">Dismiss</span>
-            <XMarkIcon className="h-5 w-5 text-white" aria-hidden="true" />
+            <XMarkIcon className="h-5 w-5 text-white dark:text-black" aria-hidden="true" />
           </button>
         </div>
       </div>
